4c5293b82eaa46804cb5089bcfe488c918776695
imemmul <emrulurak17@outlook.com>
2023-05-01 16:00:15 +0300

    task_2 maximum_expression

--- Task2/arithmetic.py
+++ Task2/arithmetic.py
@@ -8,8 +8,12 @@ def eval(a, b, op):
     elif op == '-':
         return a - b
 
-def take_string(operand_1, operand_2, op, value):
-    return str(operand_1) + op + str(operand_2) + "--->" + str(value)
+# def take_string(operand_1, operand_2, op, value):
+#     return str(operand_1) + op + str(operand_2) + "--->" + str(value)
+
+import sys
+
+import sys
 
 def calculate(exp):
     length = len(exp)
@@ -18,13 +22,13 @@ def calculate(exp):
     # max_list = [tuple([[0] * n]) for i in range(n)] # creating max_list that keeps max evaluations
     min_list = [[0]*n for i in range(n)]
     max_list = [[0]*n for i in range(n)]
-    # print(max_list)
-    max_expression = []
     for i in range(n):
-        # setting numbers to dioganol location
+        # setting numbers to diagonal location
         min_list[i][i] = int(exp[2*i]) 
         max_list[i][i] = int(exp[2*i])
-    # print(max_list)
+    max_expression = [["" for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        max_expression[i][i] = str(exp[2*i])
     for s in range(n - 1):
         for i in range(n - s - 1):
             j = i + s + 1
@@ -39,29 +43,24 @@ def calculate(exp):
                 b = eval(min_list[i][k], max_list[k + 1][j], op)
                 c = eval(max_list[i][k], min_list[k + 1][j], op)
                 d = eval(max_list[i][k], max_list[k + 1][j], op)
+                # TODO try to print expression that finds maximum value
+                temp_max = max_val
                 min_val = min(min_val, a, b, c, d)
                 max_val = max(max_val, a, b, c, d)
-                # TODO try to print expression that finds maximum value
-                if max_val == a:
-                    max_expression.append(take_string(min_list[i][k], min_list[k + 1][j], op, a))
-                elif max_val == b:
-                    max_expression.append(take_string(min_list[i][k], max_list[k + 1][j], op, b))
-                elif max_val == c:
-                    max_expression.append(take_string(max_list[i][k], min_list[k + 1][j], op, c))
-                elif max_val == d:
-                    max_expression.append(take_string(max_list[i][k], max_list[k + 1][j], op, d))
-                else:
-                    pass # max_value didn't change
+                if max_val != temp_max: # if there is a change in max_value add to max_exression
+                    max_expression[i][j] += f"({max_expression[i][k]}{op}{max_expression[k+1][j]})"
             min_list[i][j] = min_val
             max_list[i][j] = max_val
-    # print(max_expression)
-    return max_list[0][n - 1]
+    return max_list[0][n - 1], max_expression[0][n - 1]
 
 if __name__ == "__main__":
     try:
         filename = sys.argv[-1]
         file = open(filename, "r")
         exp = file.readlines()
-        print(calculate(exp[0]))
+        value, max_exp = calculate(exp[0])
+        print(f"maximum-value: {value}")
+        print("-" * 50)
+        print(f"maximum expression that finds the value: {max_exp}")
     except:
-        print(f"Check your file name! \nUSAGE python arithmetic.py filename.txt")
+        print(f"Check your file name! \nUSAGE python arithmetic.py filename.txt or there is another problem with code!!")
